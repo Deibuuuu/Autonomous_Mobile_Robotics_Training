24713a3f9600868a36cedbcea00b6596ee7fe31a
Deibuuuu <davem.alver@gmail.com>
2026-04-22 15:27:02 +0800

    finished phase 1

--- constants.h
+++ constants.h
@@ -3,24 +3,24 @@
 #include <Arduino.h>
 
 // --- Front Left Motor ---
-const int FL_ENA = 2; 
-const int FL_IN1 = 22;
-const int FL_IN2 = 23;
+const int FL_ENA = 5; 
+const int FL_IN1 = 7;
+const int FL_IN2 = 8;
 
 // --- Front Right Motor ---
-const int FR_ENB = 3; 
-const int FR_IN3 = 24;
-const int FR_IN4 = 25;
+const int FR_ENB = 6; 
+const int FR_IN3 = 11;
+const int FR_IN4 = 24;
 
 // --- Back Left Motor ---
-const int BL_ENA = 4; 
-const int BL_IN1 = 26;
-const int BL_IN2 = 27;
+const int BL_ENA = 9; 
+const int BL_IN1 = 22;
+const int BL_IN2 = 23;
 
 // --- Back Right Motor ---
-const int BR_ENB = 5; 
-const int BR_IN3 = 28;
-const int BR_IN4 = 29;
+const int BR_ENB = 10; 
+const int BR_IN3 = A4;
+const int BR_IN4 = A5;
 
 // --- Test Settings ---
 const float TEST_SPEED = 0.5; // 50% Power
